26c557a66559d418f208f2d0ca9c880e8213bfaf
zen <zen.nguyen@moodys.com>
2019-11-12 17:09:39 -0800

    color preservation version from base project from pytoch, plus json output return

---
+++ neural_style/utils.py
@@ -0,0 +1,66 @@
+import torch
+from PIL import Image
+import cv2
+
+# Load image file
+def load_image_bgr(path):
+    # Images loaded as BGR
+    img = cv2.imread(path)
+    return img
+def save_img_bgr(image_path, img):
+    img = img.clip(0, 255)
+    cv2.imwrite(image_path, img)
+# https://github.com/rrmina/fast-neural-style-pytorch/blob/master/utils.py
+def transfer_color(src, dest):
+    """
+    Transfer Color using YIQ colorspace. Useful in preserving colors in style transfer.
+    This method assumes inputs of shape [Height, Width, Channel] in BGR Color Space
+    """
+    src, dest = src.clip(0,255), dest.clip(0,255)
+        
+    # Resize src to dest's size
+    H,W,_ = src.shape 
+    dest = cv2.resize(dest, dsize=(W, H), interpolation=cv2.INTER_CUBIC)
+    
+    dest_gray = cv2.cvtColor(dest, cv2.COLOR_BGR2GRAY) #1 Extract the Destination's luminance
+    src_yiq = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)   #2 Convert the Source from BGR to YIQ/YCbCr
+    src_yiq[...,0] = dest_gray                         #3 Combine Destination's luminance and Source's IQ/CbCr
+    
+    return cv2.cvtColor(src_yiq, cv2.COLOR_YCrCb2BGR).clip(0,255)  #4 Convert new image from YIQ back to BGR
+
+def load_image(filename, size=None, scale=None):
+    img = Image.open(filename)
+    if size is not None:
+        img = img.resize((size, size), Image.ANTIALIAS)
+    elif scale is not None:
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+        
+    # In case of grayScale images the len(img.shape) == 2
+#     if len(img.shape) > 2 and img.shape[2] == 4:
+#         #convert the image from RGBA2RGB
+#         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)        
+    img = img.convert('RGB')
+    return img
+
+
+def save_img_rgb(filename, data):
+    img = data.clone().clamp(0, 255).numpy()
+    img = img.transpose(1, 2, 0).astype("uint8")
+    img = Image.fromarray(img)
+    img.save(filename)
+
+
+def gram_matrix(y):
+    (b, ch, h, w) = y.size()
+    features = y.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
+
+
+def normalize_batch(batch):
+    # normalize using imagenet mean and std
+    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
+    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+    batch = batch.div_(255.0)
+    return (batch - mean) / std
